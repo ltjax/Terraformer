3ec63838944d17634f3f4bd6cc9e81c835b4212a
Marius Elvert <marius.elvert@softwareschneiderei.de>
2018-01-26 22:23:12 +0100

    Fixed function calls

---
+++ entities.lua
@@ -0,0 +1,22 @@
+local class = require 'middleclass'
+
+local Entities = class 'Entities'
+
+function Entities:initialize()
+  self.list = {}
+end
+
+function Entities:add(entity)
+  table.insert(self.list, entity)
+end
+
+function Entities:callAll(functionName, ...)
+  for _, entity in self.list do
+    local f = entity[functionName]
+    if f then
+      f(f, unpack(arg))
+    end
+  end
+end
+
+return Entities
--- ingamestate.lua
+++ ingamestate.lua
@@ -1,9 +1,14 @@
 
-
 local InGameState = {}
 
 function InGameState:init()
     self.entities = {}
+    self:insertEntity({
+        draw=function(self)
+            love.graphics.setColor(0, 100, 100)
+            love.graphics.rectangle("fill", 10, 10, 120, 80)
+        end
+    })
 end
 
 function InGameState:insertEntity(entity)
@@ -13,13 +18,13 @@ end
 function InGameState:draw()
     for _, entity in ipairs(self.entities) do
         if entity.drawBackground then
-            entity.drawBackground()
+            entity:drawBackground()
         end
     end
 
     for _, entity in ipairs(self.entities) do
         if entity.draw then
-            entity.draw()
+            entity:draw()
         end
     end
 end
@@ -27,7 +32,7 @@ end
 function InGameState:update(dt)
     for _, entity in ipairs(self.entities) do
         if entity.update then
-            entity.update()
+            entity:update()
         end
     end
 end
@@ -39,7 +44,7 @@ function InGameState:keypressed(key)
     if key == "space" then
         for _, entity in ipairs(self.entities) do
             if entity.step then
-                entity.step()
+                entity:step()
             end
         end
     end
