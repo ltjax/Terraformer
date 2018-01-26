01531fa00036078ca0535573c79d0dbe6544f5c0
Marius Elvert <marius.elvert@softwareschneiderei.de>
2018-01-26 22:58:27 +0100

    Using new entities class

--- ingamestate.lua
+++ ingamestate.lua
@@ -4,7 +4,7 @@ local Player = require "player"
 local InGameState = {}
 
 function InGameState:init()
-    self.entities = {}
+    self.entities = Entities:new()
     self:insertEntity({
         draw=function(self)
             love.graphics.setColor(0, 100, 100)
@@ -14,29 +14,16 @@ function InGameState:init()
 end
 
 function InGameState:insertEntity(entity)
-    table.insert(self.entities, entity)
+    self.entities:add(entity)
 end
 
 function InGameState:draw()
-    for _, entity in ipairs(self.entities) do
-        if entity.drawBackground then
-            entity:drawBackground()
-        end
-    end
-
-    for _, entity in ipairs(self.entities) do
-        if entity.draw then
-            entity:draw()
-        end
-    end
+    self.entities:callAll('draw')
+    self.entities:callAll('drawBackground')
 end
 
 function InGameState:update(dt)
-    for _, entity in ipairs(self.entities) do
-        if entity.update then
-            entity:update()
-        end
-    end
+    self.entities:callAll('update')
 end
 
 function InGameState:keypressed(key)
@@ -44,11 +31,7 @@ function InGameState:keypressed(key)
         love.event.quit()
     end
     if key == "space" then
-        for _, entity in ipairs(self.entities) do
-            if entity.step then
-                entity:step()
-            end
-        end
+        self.entities:callAll('step')
     end
 end
 
