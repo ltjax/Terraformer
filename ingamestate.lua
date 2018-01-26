c6530aba8a1697cb6c2885a4749069de2f8b5ecc
Marius Elvert <marius.elvert@softwareschneiderei.de>
2018-01-26 22:10:39 +0100

    basic entity call delegation

--- ingamestate.lua
+++ ingamestate.lua
@@ -3,20 +3,45 @@
 local InGameState = {}
 
 function InGameState:init()
+    self.entities = {}
+end
 
+function InGameState:insertEntity(entity)
+    table.insert(self.entities, entity)
 end
 
 function InGameState:draw()
+    for _, entity in ipairs(self.entities) do
+        if entity.drawBackground then
+            entity.drawBackground()
+        end
+    end
 
+    for _, entity in ipairs(self.entities) do
+        if entity.draw then
+            entity.draw()
+        end
+    end
 end
 
 function InGameState:update(dt)
-
+    for _, entity in ipairs(self.entities) do
+        if entity.update then
+            entity.update()
+        end
+    end
 end
 
 function InGameState:keypressed(key)
     if key == "escape" then
         love.event.quit()
+    end
+    if key == "space" then
+        for _, entity in ipairs(self.entities) do
+            if entity.step then
+                entity.step()
+            end
+        end
     end
 end
 
