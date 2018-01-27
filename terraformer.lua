13378a8329a9e57de869addb218c910cb854e67b
Fabian Sperber <fabian.sperber@gmx.net>
2018-01-27 15:47:08 +0100

    In-/decrease terraformer's radius according to energy level

--- terraformer.lua
+++ terraformer.lua
@@ -9,6 +9,9 @@ TerraFormer.static.max_energy = 40
 TerraFormer.static.energy_cost = 0.4 -- per second
 TerraFormer.static.segments = 100
 TerraFormer.static.mineral_cost = 100
+TerraFormer.static.shield_radius_increase = 0.5 -- per second
+TerraFormer.static.shield_radius_min = 2
+TerraFormer.static.shield_radius_max = 15
 
 function TerraFormer:initialize(eventBus, posx, posy)
     EnergyTransmitter.initialize(self)
@@ -17,15 +20,21 @@ function TerraFormer:initialize(eventBus, posx, posy)
     self.energy = 0
     self.active = false
     self.generated = 0
-    self.active_radius = 15.0
+    self.active_radius = TerraFormer.shield_radius_min
 end
 
 function TerraFormer:draw(camera)
   love.graphics.push()
     love.graphics.setColor(0, 255, 0, 255)
     love.graphics.circle("fill", self.position.x, self.position.y, 0.5)
+    if self.active and self.active_radius < TerraFormer.shield_radius_max then
+        love.graphics.setLineWidth(0.05);
+        love.graphics.setColor(0, 255, 0, 150)
+        love.graphics.circle("line", self.position.x, self.position.y, self.active_radius, TerraFormer.segments)
+    end
   love.graphics.pop();
-  camera:drawText(mathhelpers.percentagestring(self:potential()), self.position.x, self.position.y)
+  local p = (self.active_radius - TerraFormer.shield_radius_min) / (TerraFormer.shield_radius_max - TerraFormer.shield_radius_min)
+  camera:drawText(mathhelpers.percentagestring(p), self.position.x, self.position.y)
 end
 
 function TerraFormer:drawBackground()
@@ -45,8 +54,12 @@ function TerraFormer:update(dt)
         self.energy = self.energy - usage
         self.active = true
         self.generated = self.generated + dt
+        self.active_radius = math.min(TerraFormer.shield_radius_max, self.active_radius + TerraFormer.shield_radius_increase * dt)
     else
-        self.active = false
+        self.active_radius = math.max(TerraFormer.shield_radius_min, self.active_radius - TerraFormer.shield_radius_increase * dt)
+        if self.active_radius <= TerraFormer.shield_radius_min then
+            self.active = false
+        end
     end
     
     while self.generated > 1 do
