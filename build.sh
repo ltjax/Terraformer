395fd792e4660f03e20f209af1669f96696e4de4
Marius Elvert <marius.elvert@softwareschneiderei.de>
2018-01-28 14:55:41 +0100

    Added build files

---
+++ build.sh
@@ -0,0 +1,12 @@
+cd game
+rm Terraformer.love || :
+zip -9 -r Terraformer.love .
+mv Terraformer.love ../
+cd ..
+cat love_windows/love.exe Terraformer.love > Terraformer.exe
+rm -R terraformer
+mkdir -p terraformer/
+cd love_windows
+cp ../Terraformer.exe SDL2.dll OpenAL32.dll license.txt love.dll lua51.dll mpg123.dll msvcp120.dll msvcr120.dll ../terraformer
+cd ..
+zip -9 -r Terraformer.zip terraformer/
